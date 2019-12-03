9197df4d1bdfaf05a3342c978f6d0ae4a94db476
AMITABH <amitabhcse01@gmail.com>
2019-12-03 12:46:44 +0600

    IPhone X/XS/11 Pro-21, IPhone X/XS/11 Pro-22

---
+++ app/src/main/java/com/uysys/uys/ourPartner/OurPartnerActivity.java
@@ -0,0 +1,15 @@
+package com.uysys.uys.ourPartner;
+
+import android.os.Bundle;
+import com.uysys.uylab.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OurPartnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_our_partner);
+    }
+}
---
+++ app/src/main/java/com/uysys/uys/setting/SettingActivity.java
@@ -0,0 +1,17 @@
+package com.uysys.uys.setting;
+
+import android.os.Bundle;
+
+
+import com.uysys.uylab.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
