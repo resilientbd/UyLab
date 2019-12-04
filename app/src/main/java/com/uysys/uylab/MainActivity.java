b6494a23ad245dbad7c3db9764bd65c69f231007
User <mdraihanpat1996@gmail.com>
2019-12-04 16:38:36 +0600

    iPhone X/XS/11 Pro – 23 iPhone X/XS/11 Pro – 24

--- app/src/main/java/com/uysys/uylab/MainActivity.java
+++ app/src/main/java/com/uysys/uylab/MainActivity.java
@@ -1,14 +1,32 @@
 package com.uysys.uylab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public TextView facebok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        facebok = findViewById(R.id.TextFacebook);
+
+        facebok.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()== R.id.TextFacebook){
+            Intent intent = new Intent(this,Main2Activity.class);
+            startActivity(intent);
+        }
     }
 }
