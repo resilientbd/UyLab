4ea6a6e54958e3cc1bbe930ccefad1e3605f1bcd
fahadshuvo33 <fahadshuvo33@gmail.com>
2019-12-09 10:57:46 +0600

    done

---
+++ app/src/main/java/com/uysys/uylab/save/Save_Activity.java
@@ -0,0 +1,29 @@
+package com.uysys.uylab.save;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uysys.uylab.R;
+
+public class Save_Activity extends AppCompatActivity {
+    private Button btn1;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.save );
+
+        btn1=findViewById ( R.id.goBtn_Save );
+        btn1.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent ( Save_Activity.this,null );
+            }
+        } );
+    }
+}
