3620c57e7565d79fd67680db8f3b579b15dcdc23
resilientbd <faisal.hossain.pk@gmail.com>
2019-12-12 05:16:33 +0600

    init

---
+++ app/src/main/java/com/uysys/uylab/ui/profile/profile.java
@@ -0,0 +1,17 @@
+package com.uysys.uylab.ui.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import com.uysys.uylab.R;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
