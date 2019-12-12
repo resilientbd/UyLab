a934e2eb846ba17308a8b670b68ef5f9afcfcdb3
shekhfarid <shekhfarid209@gmail.com>
2019-12-12 13:58:58 +0600

    all done

--- app/src/main/java/com/uysys/uylab/test1/Test1Activity.java
+++ app/src/main/java/com/uysys/uylab/test1/Test1Activity.java
@@ -1,14 +1,16 @@
-package com.uysys.uylab;
+package com.uysys.uylab.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.uysys.uylab.R;
+
+public class Test1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test1);
     }
 }
