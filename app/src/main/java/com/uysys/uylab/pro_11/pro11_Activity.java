2c262b04fc5b29c339e0160ca6a289f43bca83b0
fahadshuvo33 <fahadshuvo33@gmail.com>
2019-12-12 17:06:28 +0600

    Merge remote-tracking branch 'remotes/origin/v1/merge2' into v1/fahad
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/uysys/uys/mentors/MentorsActivity.java
    #	app/src/main/res/drawable/button_shape.xml
    #	app/src/main/res/drawable/image_shaper_circle.xml
    #	app/src/main/res/drawable/notificationshower.xml
    #	app/src/main/res/drawable/text_view_shape.xml
    #	app/src/main/res/layout/avail_offer_2.xml
    #	app/src/main/res/layout/avail_offer_3.xml

---
+++ app/src/main/java/com/uysys/uylab/pro_11/pro11_Activity.java
@@ -0,0 +1,21 @@
+package com.uysys.uylab.pro_11;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uysys.uylab.R;
+
+public class pro11_Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.pro11 );
+    }
+}
