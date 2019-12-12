3c3ccdd35ad4018b1d8e5353598f9f8f734f783b
Riad Hossain <khanriad215@gmail.com>
2019-12-12 17:01:21 +0600

    Student Login Done

---
+++ app/src/main/java/com/uysys/uylab/student_login/Student_LoginActivity.java
@@ -0,0 +1,16 @@
+package com.uysys.uylab.student_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uysys.uylab.R;
+
+public class Student_LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student__login);
+    }
+}
