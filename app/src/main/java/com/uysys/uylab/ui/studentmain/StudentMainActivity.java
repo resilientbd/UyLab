9d0c2b29551e64ddc12d041a775541bc15d647c7
resilientbd <faisal.hossain.pk@gmail.com>
2019-12-14 13:32:03 +0600

    update

---
+++ app/src/main/java/com/uysys/uylab/ui/studentmain/StudentMainActivity.java
@@ -0,0 +1,17 @@
+package com.uysys.uylab.ui.studentmain;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uysys.uylab.R;
+
+public class StudentMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_main);
+
+    }
+}
