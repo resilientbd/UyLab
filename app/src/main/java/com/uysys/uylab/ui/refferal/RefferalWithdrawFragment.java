95f4f95e1fb917f700be1adf46bfda6617fd89c5
Shakiba <shakinur1995@gmail.com>
2019-12-14 20:08:41 +0600

    refferal page done

---
+++ app/src/main/java/com/uysys/uylab/ui/refferal/RefferalWithdrawFragment.java
@@ -0,0 +1,23 @@
+package com.uysys.uylab.ui.refferal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.uysys.uylab.R;
+
+public class RefferalWithdrawFragment extends Fragment {
+     private View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_refferal_withdraw, container, false);
+        return view;
+    }
+
+}
