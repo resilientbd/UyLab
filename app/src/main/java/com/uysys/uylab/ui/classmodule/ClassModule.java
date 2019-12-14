a933582ba3550142b5bf6b7ac1039077dd222e29
resilientbd <faisal.hossain.pk@gmail.com>
2019-12-14 12:25:40 +0600

    update

---
+++ app/src/main/java/com/uysys/uylab/ui/classmodule/ClassModule.java
@@ -0,0 +1,16 @@
+package com.uysys.uylab.ui.classmodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uysys.uylab.R;
+
+public class ClassModule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_module);
+    }
+}
