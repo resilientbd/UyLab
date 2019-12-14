936ff87f50abe6e4e039413d0bf215409e3099a5
resilientbd <faisal.hossain.pk@gmail.com>
2019-12-14 14:06:51 +0600

    student login update

--- app/src/main/java/com/uysys/uylab/ui/student_login/Student_LoginActivity.java
+++ app/src/main/java/com/uysys/uylab/ui/student_login/Student_LoginActivity.java
@@ -2,9 +2,12 @@ package com.uysys.uylab.ui.student_login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.uysys.uylab.R;
+import com.uysys.uylab.ui.studentmain.StudentMainActivity;
 
 public class Student_LoginActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class Student_LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student__login);
+    }
+
+    public void loginStudent(View view) {
+        Intent intent=new Intent(Student_LoginActivity.this, StudentMainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
