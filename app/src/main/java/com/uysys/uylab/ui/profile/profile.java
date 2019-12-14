b91cc0d823bd0ed160a1b8a943c33d257a52a5e0
Shakiba <shakinur1995@gmail.com>
2019-12-14 18:08:27 +0600

    profile edit, refefral page

--- app/src/main/java/com/uysys/uylab/ui/profile/profile.java
+++ app/src/main/java/com/uysys/uylab/ui/profile/profile.java
@@ -2,10 +2,13 @@ package com.uysys.uylab.ui.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 import com.uysys.uylab.R;
+import com.uysys.uylab.ui.profile1.Profile1Activity;
 
 public class profile extends AppCompatActivity {
 
@@ -13,5 +16,9 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void GoToEditProfile(View view) {
+        startActivity(new Intent(this, Profile1Activity.class));
     }
 }
