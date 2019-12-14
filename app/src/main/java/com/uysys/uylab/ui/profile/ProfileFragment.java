6563c153f07ea665a23f2caf6251074221938c98
Shakiba <shakinur1995@gmail.com>
2019-12-14 18:32:06 +0600

    profilefragment done

---
+++ app/src/main/java/com/uysys/uylab/ui/profile/ProfileFragment.java
@@ -0,0 +1,39 @@
+package com.uysys.uylab.ui.profile;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+import com.uysys.uylab.R;
+import com.uysys.uylab.ui.profile1.Profile1Activity;
+
+public class ProfileFragment extends Fragment {
+
+    private View view;
+    private Button btneditprofile;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_profile, container, false);
+        btneditprofile=(Button)view.findViewById(R.id.editprofilebtn);
+        btneditprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return view;
+
+    }
+
+
+}
