755b618cd07b61c26c99bebc82651f49e9130445
Mim-web <julfamim@gmail.com>
2019-12-18 16:12:33 +0600

    v5/julfa_rouf (JobApply,JobOpen,Job)

---
+++ app/src/main/java/com/uysys/uylab/ui/job/JobActivity.java
@@ -0,0 +1,16 @@
+package com.uysys.uylab.ui.job;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uysys.uylab.R;
+
+public class JobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job);
+    }
+}
