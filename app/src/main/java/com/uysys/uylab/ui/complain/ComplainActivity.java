94f572806f65e1b98f51afdfc5ee4cd7211b0170
zakaria90024 <zakaria112203@gmail.com>
2019-12-18 17:04:27 +0600

    Deshbord contest Done

---
+++ app/src/main/java/com/uysys/uylab/ui/complain/ComplainActivity.java
@@ -0,0 +1,16 @@
+package com.uysys.uylab.ui.complain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uysys.uylab.R;
+
+public class ComplainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complain);
+    }
+}
