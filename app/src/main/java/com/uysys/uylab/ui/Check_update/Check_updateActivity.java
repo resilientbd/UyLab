e0978d00c93c674e46a2e797e0b0e64ed0129208
Raiham878 <mdraihanpat1996@gmail.com>
2019-12-19 17:14:57 +0600

    check update Seminar

---
+++ app/src/main/java/com/uysys/uylab/ui/Check_update/Check_updateActivity.java
@@ -0,0 +1,16 @@
+package com.uysys.uylab.ui.Check_update;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uysys.uylab.R;
+
+public class Check_updateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_update);
+    }
+}
---
+++ app/src/main/java/com/uysys/uylab/ui/Seminar/SeminarActivity.java
@@ -0,0 +1,16 @@
+package com.uysys.uylab.ui.Seminar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uysys.uylab.R;
+
+public class SeminarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seminar);
+    }
+}
