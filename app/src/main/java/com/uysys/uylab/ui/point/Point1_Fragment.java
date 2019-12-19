30afe819f70bb6d20fb25d287011be7d15ed1893
earhossain13@gmail.com <01955179507ab>
2019-12-19 17:23:37 +0600

    v5/yarhossain (point,point1)complete point2 70%

--- app/src/main/java/com/uysys/uylab/ui/point/Point1_Fragment.java
+++ app/src/main/java/com/uysys/uylab/ui/point/Point1_Fragment.java
@@ -3,6 +3,8 @@ package com.uysys.uylab.ui.point;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,12 +15,19 @@ import com.uysys.uylab.R;
 import com.uysys.uylab.ui.base.BaseFragment;
 
 public class Point1_Fragment extends BaseFragment {
+    private Adapter_Points1 adapter_points1;
+    private RecyclerView recyclerView;
 
     View view;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view= inflater.inflate(R.layout.activity_point1,container,false);
+        recyclerView = view.findViewById(R.id.points1_recycler);
+        adapter_points1 = new Adapter_Points1();
+        recyclerView.setAdapter(adapter_points1);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 1));
+
         return view;
     }
 }
--- app/src/main/java/com/uysys/uylab/ui/point/Point_Fragment.java
+++ app/src/main/java/com/uysys/uylab/ui/point/Point_Fragment.java
@@ -3,6 +3,8 @@ package com.uysys.uylab.ui.point;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,12 +14,55 @@ import android.view.ViewGroup;
 import com.uysys.uylab.R;
 import com.uysys.uylab.ui.base.BaseFragment;
 
-public class Point_Fragment extends BaseFragment {
+public class Point_Fragment extends BaseFragment implements View.OnClickListener {
+    private Adapter_Point adapter_point;
+    private RecyclerView recyclerView;
+    private View statusBtn;
+    private View reedemBtn;
+    private View pointBtn;
 
     View view;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       view=inflater.inflate(R.layout.activity_point,container,false);
+        view = inflater.inflate(R.layout.activity_point, container, false);
+        recyclerView = view.findViewById(R.id.points_recycler);
+        adapter_point = new Adapter_Point();
+        recyclerView.setAdapter(adapter_point);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 1));
+
+        statusBtn = view.findViewById(R.id.statusbtn);
+        reedemBtn = view.findViewById(R.id.reedembtn);
+        pointBtn = view.findViewById(R.id.pointbtn);
+
+        statusBtn.setOnClickListener(this);
+        reedemBtn.setOnClickListener(this);
+        pointBtn.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        BaseFragment fragment=null;
+        switch (v.getId()) {
+            case R.id.statusbtn:
+                fragment=new Point_Fragment();
+                break;
+            case R.id.reedembtn:
+                fragment=new Point1_Fragment();
+                break;
+            case R.id.pointbtn:
+                fragment=new Point1_Fragment();
+                break;
+
+            default:
+                break;
+        }
+
+        if (getFragmentListener()!=null&&fragment!=null)
+        {
+            fragment.setFragmentListener(getFragmentListener());
+            getFragmentListener().onAddFragment(fragment);
+        }
     }
 }
