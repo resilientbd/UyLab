bde91c5e44a9ebc60cd5186a76d0bf2225c41ef9
AMITABH <amitabhcse01@gmail.com>
2019-12-19 01:58:43 -0800

    amitabh_new
    live_class
    complete with connection and recyler

--- app/src/main/java/com/uysys/uylab/ui/liveClass/Live_Class_Video.java
+++ app/src/main/java/com/uysys/uylab/ui/liveClass/Live_Class_Video.java
@@ -1,6 +1,5 @@
 package com.uysys.uylab.ui.liveClass;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,39 +7,32 @@ import android.view.ViewGroup;
 
 import com.uysys.uylab.R;
 import com.uysys.uylab.ui.base.BaseFragment;
-import com.uysys.uylab.ui.mentors.MentorsAdapter;
-import com.uysys.uylab.ui.mentors.MentorsProfileActivity;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Live_Class extends BaseFragment implements Live_Class_Adapter.Live_Class_AdapterClickListener {
+public class Live_Class_Video extends BaseFragment implements Live_Video_Adapter.Live_Video_AdapterClickListener {
+
     private View view;
     private RecyclerView recyclerView;
-    private Live_Class_Adapter live_class_adapter;
+    private Live_Video_Adapter live_video_adapter;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        view = inflater.inflate(R.layout.activity_live_class,container,false);
-        live_class_adapter = new Live_Class_Adapter();
+         view = inflater.inflate(R.layout.fragment_live_class_video,container,false);
+        live_video_adapter = new Live_Video_Adapter();
         recyclerView=view.findViewById(R.id.recylerviewnew);
-        recyclerView.setAdapter(live_class_adapter);
+        recyclerView.setAdapter(live_video_adapter);
 
         recyclerView.setLayoutManager(new LinearLayoutManager((getActivity())));
-
-        live_class_adapter.setLive_class_adapterClickListener(this);
+        live_video_adapter.setLive_video_adapterClickListener(this);
 
         return view;
-
     }
 
     @Override
-    public void onLive_classItemClick(int position) {
-//        Intent intent=new Intent(getActivity(), Live_Class_Video.class);
-//        startActivity(intent);
+    public void onLive_VideoItemClick(int position) {
         if(getFragmentListener()!=null)
         {
             Live_Class_Video frag=new Live_Class_Video();
---
+++ app/src/main/java/com/uysys/uylab/ui/tutorial/Fragment_Tutorial.java
@@ -0,0 +1,21 @@
+package com.uysys.uylab.ui.tutorial;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uysys.uylab.R;
+import com.uysys.uylab.ui.base.BaseFragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Fragment_Tutorial  extends BaseFragment {
+    private View view;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_tutorial,container,false);
+        return view;
+    }
+}
