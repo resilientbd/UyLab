2fb7229aa60216be6611fc94a53fdeb701dd0013
Android & App <peyas16bd@yahoo.com>
2019-12-19 12:45:15 +0600

    Create Terms And Conditions Fragment

---
+++ app/src/main/java/com/uysys/uylab/ui/Terms_And_Conditions/TermsAndConditions_Fragment.java
@@ -0,0 +1,16 @@
+package com.uysys.uylab.ui.Terms_And_Conditions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uysys.uylab.R;
+
+public class TermsAndConditions_Fragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_conditions__fragment);
+    }
+}
