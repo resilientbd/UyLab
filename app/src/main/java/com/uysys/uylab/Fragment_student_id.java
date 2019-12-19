4a0d1cf34a6af8e97b471b3035727eb2f31aebf2
faisal-hub <faisalahmedf19@gmail.com>
2019-12-19 16:59:54 +0600

    v5/faisal (student login)

---
+++ app/src/main/java/com/uysys/uylab/Fragment_student_id.java
@@ -0,0 +1,14 @@
+package com.uysys.uylab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fragment_student_id extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_student_id);
+    }
+}
