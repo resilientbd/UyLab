78b5acda297dc8b081180ecb977bacdc5cabd909
Android & App <peyas16bd@yahoo.com>
2019-12-19 16:29:04 +0600

    Create FAQ_shower-View  Recycler

--- app/src/main/java/com/uysys/uylab/ui/Terms_And_Conditions/TermsAndConditions_Fragment.java
+++ app/src/main/java/com/uysys/uylab/ui/Terms_And_Conditions/TermsAndConditions_Fragment.java
@@ -1,16 +1,37 @@
 package com.uysys.uylab.ui.Terms_And_Conditions;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.uysys.uylab.R;
+import com.uysys.uylab.ui.classContent.ClassContentAdapter;
+import com.uysys.uylab.ui.classContent.Class_Content_Activity;
+import com.uysys.uylab.ui.classContentVideoShower.Class_Content_Video_Shower_Activity;
 
-public class TermsAndConditions_Fragment extends AppCompatActivity {
+public class TermsAndConditions_Fragment extends AppCompatActivity implements FAQAdapter.OnClassAdapterItemClickListener {
+
+    private RecyclerView mRecyclerView;
+    private TextView headerText;
+    private ClassContentAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_conditions__fragment);
+        mRecyclerView=findViewById(R.id.recycleView);
+        headerText=findViewById(R.id.faqHeader);
+
+    }
+
+
+    @Override
+    public void onClassAdapterItemClick(int position) {
+
     }
 }
